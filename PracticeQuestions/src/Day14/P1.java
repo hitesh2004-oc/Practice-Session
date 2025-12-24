375db3d9d9a51e650c1eb6dee7dbf82a25bb3947
Hitesh Malviya <hiteshmalviya955@gmail.com>
2025-12-24 14:55:29 +0530

    Day14

---
+++ PracticeQuestions/src/Day14/P1.java
@@ -0,0 +1,50 @@
+package Day14;
+import java.io.File;
+//..........File Handling..........//
+public class P1 {
+	public static void main (String[] args) {
+		// File Operation //
+     //(1) Create File ...[You Have to Referesh Main Package]
+	    File f = new File("Hitesh.txt");
+	    File a = new File("Angular.txt");
+	    File b = new File(".txt");
+	    File c = new File("Angular.txt");
+	    try {
+	 	f.createNewFile();
+	    a.createNewFile();
+	    b.createNewFile();
+	    c.createNewFile();
+	    }
+		catch(Exception e){
+			System.out.println(e);
+			}
+	  //(2) Create Folder...[You Have to Referesh Main Package]
+	    File fd = new File("Data");
+	    boolean s1 = fd.mkdir(); //[Using mkdir Method]
+	    
+	    //Create File Inside the Folder
+	    File a1 = new File(f,"Test.txt");
+	    
+	    try {
+			a1.createNewFile();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	  
+	  //(3) Display All File & Directories in Folder...
+	    //Path of That Particular Folder -> D:\\TestingProject
+	    File dis = new File("D:\\TestingProject");
+		String[] arr = dis.list();
+		
+		for(String p :arr) {
+			File f1 = new File(dis,p);
+			if(f1.isFile()) {
+				System.out.println(p+"  it is File");
+			}
+			if(f1.isDirectory()) {
+				System.out.println(p+"  it is Folder");
+			}
+		}
+	      
+	}
+}
