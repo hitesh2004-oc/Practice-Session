36faf6243df9a4c74c52ea08c6bf0aa75a21a83f
Hitesh Malviya <hiteshmalviya955@gmail.com>
2026-01-13 19:49:45 +0530

    Day22

--- PracticeQuestions/src/Day22/P9.java
+++ PracticeQuestions/src/Day22/P9.java
@@ -5,7 +5,8 @@ public class P9 {
 		int n = 19;
 		for(int i = 0;i<n;i++) {
 			for(int j = 0;j<n;j++) {
-				if( i+j >=(n-1)+(n-1)/2 || i-j >= (n-1)/2||j-i>=(n-1)/2 ||i+j<=(n-1)/2 ) {
+				if( i+j >=(n-1)+(n-1)/2 || i-j >= (n-1)/2||j-i>=(n-1)/2
+						||i+j<=(n-1)/2 ) {
 					
 					System.out.print(" ");
 				}else {
